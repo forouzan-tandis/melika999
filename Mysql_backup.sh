9aef2445ef60acf7cb650ca3bd48f62027906684
melika999 <50720301+melika999@users.noreply.github.com>
2021-12-24 14:53:28 +0330

    Update Mysql_backup.sh

--- Mysql_backup.sh
+++ Mysql_backup.sh
@@ -5,35 +5,43 @@
 # edited by G Pugh 2019-01-15
 
 
-STORE_FOLDER="/opt/DIR-BACKUP"
-SOURCEFOLDER=/usr/bin/
+STORE_FOLDER="/home/MySQL-Backups"
 
 TODAY=$(date +"%Y-%m-%d")
 DAILY_DELETE_NAME="daily-"`date +"%Y-%m-%d" --date '7 days ago'`
 WEEKLY_DELETE_NAME="weekly-"`date +"%Y-%m-%d" --date '5 weeks ago'`
 MONTHLY_DELETE_NAME="monthly-"`date +"%Y-%m-%d" --date '12 months ago'`
 
-
+databases=($(/usr/bin/mysql -Bse "show databases" | grep -i -v "_schema" | grep -i -v "sys" | grep -i -v "mysql"))
 
 function do_backups() {
   # Get db name or "all"
-          #backup_db=$1
-        BACKUP_PATH=$STORE_FOLDER
-        DESTINATION=$BACKUP_PATH/daily-$TODAY.sql.gz #create a backup file using the current date in it's name
-        tar -cpzf $DESTINATION $SOURCEFOLDER #create the backup
+  backup_db=$1
 
+  # run dump
+  if [ "$backup_db" == "all" ]; then
+    BACKUP_PATH=$STORE_FOLDER/all
+    [[ ! -d "$BACKUP_PATH" ]] && mkdir -p "$BACKUP_PATH"
+  	echo "   Creating $BACKUP_PATH/daily-$TODAY.sql.gz"
+    /usr/bin/mysqldump --all-databases | gzip -9 > $BACKUP_PATH/daily-$TODAY.sql.gz
+  else
+    BACKUP_PATH=$STORE_FOLDER/$db
+    [[ ! -d "$BACKUP_PATH" ]] && mkdir -p "$BACKUP_PATH"
+  	echo "   Creating $BACKUP_PATH/daily-$TODAY.sql.gz"
+    /usr/bin/mysqldump $db | gzip -9 > $BACKUP_PATH/daily-$TODAY.sql.gz
+  fi
 
   # delete old backups
   if [ -f "$BACKUP_PATH/$DAILY_DELETE_NAME.sql.gz" ]; then
-        echo "   Deleting $BACKUP_PATH/$DAILY_DELETE_NAME.sql.gz"
-        rm -rf $BACKUP_PATH/$DAILY_DELETE_NAME.sql.gz
+  	echo "   Deleting $BACKUP_PATH/$DAILY_DELETE_NAME.sql.gz"
+  	rm -rf $BACKUP_PATH/$DAILY_DELETE_NAME.sql.gz
   fi
   if [ -f "$BACKUP_PATH/$WEEKLY_DELETE_NAME.sql.gz" ]; then
-        echo "   Deleting $BACKUP_PATH/$WEEKLY_DELETE_NAME.sql.gz"
+	echo "   Deleting $BACKUP_PATH/$WEEKLY_DELETE_NAME.sql.gz"
     rm -rf $BACKUP_PATH/$WEEKLY_DELETE_NAME.sql.gz
   fi
   if [ -f "$BACKUP_PATH/$MONTHLY_DELETE_NAME.sql.gz" ]; then
-        echo "   Deleting $BACKUP_PATH/$MONTHLY_DELETE_NAME.sql.gz"
+	echo "   Deleting $BACKUP_PATH/$MONTHLY_DELETE_NAME.sql.gz"
     rm -rf $BACKUP_PATH/$MONTHLY_DELETE_NAME.sql.gz
   fi
 
@@ -49,7 +57,7 @@ function do_backups() {
 
 }
 
-echo "*** DIR  Backups"
+echo "*** MySQL Backups"
 echo
 echo "To be deleted if present:"
 echo "   $DAILY_DELETE_NAME"
@@ -58,5 +66,11 @@ echo "   $MONTHLY_DELETE_NAME"
 echo
 
 # Entire backup
-echo "Starting DIR BACKUP"
-do_backups
+echo "Starting complete MySQL backup..."
+do_backups all
+
+# Individual db backups
+for db in "${databases[@]}"; do
+  echo "Starting $db MySQL backup..."
+  do_backups $db
+done
